b6bda252ac9b2d8dfe9225bc269d87f914f51e6e
IuliaMitch <noipasz@gmail.com>
2022-04-27 03:06:49 -0300

    CRUD completo com MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule,
+  MongooseModule.forRoot('mongodb://localhost:27017/admin')],
   controllers: [AppController,],
   providers: [AppService, ],
 })
